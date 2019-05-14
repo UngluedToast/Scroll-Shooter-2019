4b29eaaa960d6b94fd9ac865f17b760e1ed5e11c
Andrew Josey <JoganLosey@gmail.com>
2019-05-14 12:00:21 -0400

    added utilites file, and added a ground element

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
+import Sky from './Sky'
+import Ground from './Ground'
 
 const Canvas = () => {
-    const viewBox = [window.innerWidt / -2, 100 - window.innerHeight, window.innerWidth, window.innerHeight];
+    const viewBox = [window.innerWidth / -2, 100 - window.innerHeight, window.innerWidth, window.innerHeight];
     return (
         <svg 
         id="aliens-go-home-canvas"
         preserveAspectRatio="xMaxYmax none"
         viewBox={viewBox}
         >
+            <Sky />
+            <Ground />
             <circle cx={0} cy={0} r={50} />
         </svg>
     )
