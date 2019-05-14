c9a5743b8c33185ce7693b6c954d92fb9628f9e0
Andrew Josey <JoganLosey@gmail.com>
2019-05-14 11:02:00 -0400

    added basic reducer function

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import './index.css';
 import App from './App';
+import Game from './containers/Game'
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import reducer from './reducers'
+
+
+const store = createStore (
+    reducer,
+    window.__REDUX_DEVTOOLS_EXTENTSION__ && window.__REDUX_DEVTOOLS_EXTENTSION__(),
+);
+
+
+ReactDOM.render(
+    <Provider store={store}>
+        <Game />
+    </Provider>,
+    document.getElementById('root'),
+);
+
+serviceWorker.unregister()
+
+// ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
---
+++ src/reducers/index.js
@@ -0,0 +1,13 @@
+const initialState = {
+    message : ` It's easy to integrate React and Redux`,
+};
+
+
+function reducer(state = initialState) {
+    return state;
+}
+
+
+
+
+export default reducer;
